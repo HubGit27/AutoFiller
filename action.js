9117b7b301e67215b3765715b56b7ae05f07e590
brandon <brandonta121703@gmail.com>
2021-12-29 23:13:26 -0500

    About to change logic on actionjs

--- action.js
+++ action.js
@@ -1,13 +1,21 @@
+function logMessage(msg){
+    chrome.tabs.executeScript({code:"console.log('"+msg+"')"});
+}
+
 document.getElementById('fill').addEventListener('click', function(){
+    console.log("test")
     chrome.action.onClicked.addListener((tab) => {
         chrome.scripting.executeScript({
             target: {tabId: tab.id},
-            function: testautofill,
+            function: testautofill(),
         });
     });
 });
 
 function testautofill(){
-    console.log('test');
-    document.getElementById('checkout_email').value = 'test';
+    logMessage("logmessage")
+    console.log("test autofill")
+    var myvalue = "Hi";
+    document.getElementById('checkout_email').value = "hi"
+    document.getElementById('checkout_email').setAttribute('value', myvalue);
 }
